511ad69029d4b5ee39a52032f100aa557c2569e5
tsutsu <tsutsu0928@gmail.com>
2017-02-21 00:00:41 +0900

    サンプル実行バッチを複数行記述に修正．

--- test/run_test.bat
+++ test/run_test.bat
@@ -1,5 +1,11 @@
 SET test_file=myadd_test.cpp
 SET target_file=..\target\myadd.cpp
-call g++ %test_file% %target_file% -I..\googletest\googletest\include -I..\target -L..\googletest\googletest\build -lgtest_main -lgtest -o test.exe
+call g++ %test_file% %target_file% ^
+-I..\googletest\googletest\include ^
+-I..\target ^
+-L..\googletest\googletest\build ^
+-lgtest_main ^
+-lgtest ^
+-otest.exe
 call test.exe
 call del test.exe
