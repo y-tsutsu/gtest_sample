989e9842df2a1ead2773a867bf5637cbb446c12d
tsutsu <tsutsu0928@gmail.com>
2018-01-28 02:01:38 +0900

    テストコードを整形．

--- test/myadd_test.cpp
+++ test/myadd_test.cpp
@@ -1,29 +1,33 @@
 #include "gtest/gtest.h"
- 
+
 #include "myadd.h"
- 
-TEST(AddTest, PositiveNumber) {
-  EXPECT_EQ(3,  mytest::myadd(1, 2));
-  EXPECT_EQ(13, mytest::myadd(10, 3));
-  EXPECT_EQ(20, mytest::myadd(9, 11));
+
+TEST(AddTest, PositiveNumber)
+{
+    EXPECT_EQ(3, mytest::myadd(1, 2));
+    EXPECT_EQ(13, mytest::myadd(10, 3));
+    EXPECT_EQ(20, mytest::myadd(9, 11));
 }
- 
-TEST(AddTest, NegativeNumber) {
-  EXPECT_EQ(-3,  mytest::myadd(-1, -2));
-  EXPECT_EQ(-13, mytest::myadd(-10, -3));
-  EXPECT_EQ(-20, mytest::myadd(-9, -11));
+
+TEST(AddTest, NegativeNumber)
+{
+    EXPECT_EQ(-3, mytest::myadd(-1, -2));
+    EXPECT_EQ(-13, mytest::myadd(-10, -3));
+    EXPECT_EQ(-20, mytest::myadd(-9, -11));
 }
- 
-TEST(AddTest, ZeroNumber) {
-  EXPECT_EQ(0, mytest::myadd(0, 0));
-  EXPECT_EQ(1, mytest::myadd(1, 0));
-  EXPECT_EQ(2, mytest::myadd(0, 2));
-  EXPECT_EQ(-1, mytest::myadd(-1, 0));
-  EXPECT_EQ(-2, mytest::myadd(-0, -2));
+
+TEST(AddTest, ZeroNumber)
+{
+    EXPECT_EQ(0, mytest::myadd(0, 0));
+    EXPECT_EQ(1, mytest::myadd(1, 0));
+    EXPECT_EQ(2, mytest::myadd(0, 2));
+    EXPECT_EQ(-1, mytest::myadd(-1, 0));
+    EXPECT_EQ(-2, mytest::myadd(-0, -2));
 }
- 
-TEST(AddTest, PositiveAndNegativeNumber) {
-  EXPECT_EQ(0, mytest::myadd(1, -1));
-  EXPECT_EQ(1, mytest::myadd(-2, 3));
-  EXPECT_EQ(2, mytest::myadd(-3, 5));
+
+TEST(AddTest, PositiveAndNegativeNumber)
+{
+    EXPECT_EQ(0, mytest::myadd(1, -1));
+    EXPECT_EQ(1, mytest::myadd(-2, 3));
+    EXPECT_EQ(2, mytest::myadd(-3, 5));
 }
